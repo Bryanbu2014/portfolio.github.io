ec544e3c2628637dc7428cbb45fd32514e79d9b4
Bryan Bu <wen.bu@stud.th-deg.de>
2024-03-23 20:37:14 +0100

    update: ⚡ Minor changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,15 @@ document.getElementById('scrollToTop').addEventListener('click', function(event)
     document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
 });
 
-function playVideo() {
-    var iframe = document.querySelector('.responsive-iframe-vert');
-    var thumbnail = document.querySelector('.video-thumbnail');
-    
-    // Update the iframe src to force autoplay. Adjust the URL as needed for your platform
-    iframe.src += "&autoplay=1";
-    
-    // Hide the thumbnail
-    thumbnail.style.display = 'none';
-  }
+document.getElementById('emailLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link action
+    var email = this.getAttribute('data-email'); // Get the email address from data attribute
+    navigator.clipboard.writeText(email).then(function() {
+      // On success
+      alert('Email address copied: ' + email);
+    }, function(err) {
+      // On failure
+      console.error('Could not copy text: ', err);
+    });
+});  
   
