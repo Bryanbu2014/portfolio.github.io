a265b7767e7878605951b45673d7ebb14412dcf6
Bryan Bu <wen.bu@stud.th-deg.de>
2024-03-23 12:33:47 +0100

    feat: ✨ Webpage design

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ document.getElementById('scrollToTop').addEventListener('click', function(event)
     document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
 });
 
+function playVideo() {
+    var iframe = document.querySelector('.responsive-iframe-vert');
+    var thumbnail = document.querySelector('.video-thumbnail');
+    
+    // Update the iframe src to force autoplay. Adjust the URL as needed for your platform
+    iframe.src += "&autoplay=1";
+    
+    // Hide the thumbnail
+    thumbnail.style.display = 'none';
+  }
+  
